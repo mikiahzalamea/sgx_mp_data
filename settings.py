44eaabf2d5a8f2286bfb1c058ee71b4b612e9de2
mikiahzalamea <mikiah.zalamea@gmail.com>
2023-07-14 21:55:50 +0800

    Run pre-commit

--- settings.py
+++ settings.py
@@ -1,18 +1,17 @@
-import datetime 
 from datetime import date, timedelta
 
-#Adds the SGX Website as a variable to use to get Requests
-URL_BASE = 'https://www.sgx.com/research-education/derivatives'
+# Adds the SGX Website as a variable to use to get Requests
+URL_BASE = "https://www.sgx.com/research-education/derivatives"
 
-#Key for the API 5455 = July 4, 2023 so we base everything off that
+# Key for the API 5455 = July 4, 2023 so we base everything off that
 BASE_KEY = 5455
 BASE_DATE = date(2023, 7, 4)
 
-#YESTERDAY
-YESTERDAY = datetime.date.today() - datetime.timedelta(days=1)
+# YESTERDAY
+YESTERDAY = date.today() - timedelta(days=1)
 
-#FOLDER_PATH
+# FOLDER_PATH
 FOLDER_PATH_BASE = "/home/mikiah/Coding/dtl_mp_data/"
 
-#API URL
+# API URL
 API_URL = "https://links.sgx.com/1.0.0/derivatives-historical/"
